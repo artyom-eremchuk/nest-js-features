2c5e8c913ef6e09ae5cd1f0a24ff37df1d561a3d
Artyom Eremchuk <aeremchuk.dev@yandex.ru>
2025-04-05 12:09:33 +0300

    add typeorm and postgres settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,30 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import appConfig from './config/config'; // Импортируем созданный файл
+import { User } from './user/user.entity';
+import config from './config/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [appConfig],
+      load: [config],
       isGlobal: true,
+      ignoreEnvFile: false,
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          ...configService.get<TypeOrmModuleOptions>('app.database'),
+          entities: [User],
+        };
+      },
+      inject: [ConfigService],
+    }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,17 @@
 import { registerAs } from '@nestjs/config';
 
-export default registerAs('app', () => ({
-  port: process.env.PORT || 3000,
-}));
+export default registerAs('app', () => {
+  return {
+    port: process.env.PORT,
+    database: {
+      type: process.env.DB_TYPE || 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT, 10) || 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_NAME || 'database',
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: true,
+    },
+  };
+});
